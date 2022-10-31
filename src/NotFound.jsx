df301028e724ee337e4da1985de3faf8cb3a80f1
indiflex <indiflex1@gmail.com>
2022-10-31 18:41:23 +0900

    Updates

--- src/NotFound.jsx
+++
@@ -1,3 +0,0 @@
-export const NotFound = () => {
-  return <div>페이지를 찾을 수 없습니다!</div>;
-};
--- src/hooks/sample-hook.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export const useSample = () => {
-  const [sample] = useState('SAMPLE');
-
-  return sample;
-};
