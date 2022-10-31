3d7e14116fb9c4e5ef1a5bd18caa42160576f724
indiflex <indiflex1@gmail.com>
2022-10-31 18:38:17 +0900

    deploy

--- src/components/ItemLayout.jsx
+++ src/components/ItemLayout.jsx
@@ -6,7 +6,6 @@ export const ItemLayout = () => {
       <h2>ITEMS</h2>
       <Link to='/items/1'>Item1</Link>|<Link to='/items/2'>Item2</Link>
       <Outlet context={{ name: 'Hong' }} />
-      <img src='https://indiflex.github.io/reactbasic/logo192.png' alt='aaa' />
     </>
   );
 };
